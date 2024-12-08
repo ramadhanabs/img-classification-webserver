5b666caa8b833056b3b9e456d4248cdd02457565
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2024-12-08 19:50:14 +0700

    feat: add upload endpoint

--- main.cpp
+++ main.cpp
@@ -1,23 +1,64 @@
 #include "include/httplib.h"
+#include <chrono>
+#include <filesystem>
+#include <fstream>
 #include <nlohmann/json.hpp>
+#include <ostream>
 
 using json = nlohmann::json;
 using namespace std;
+namespace fs = filesystem;
 
-int main(){
-    httplib::Server svr;
+string get_unix_timestamp() {
+  auto now = chrono::system_clock::now();
+  auto duration = now.time_since_epoch();
+  auto millis = chrono::duration_cast<chrono::milliseconds>(duration).count();
 
-    svr.Get("/hello", [](const httplib::Request& req, httplib::Response& res) {
-        json response_json = {{"message", "Hello World"}};
-        res.set_content(response_json.dump(), "application/json");
-    });
+  return to_string(millis);
+}
 
-    svr.Post("/upload-image", [](const httplib::Request& req, httplib::Response& res) {
-        
-    });
+string get_file_extension(const string &filename) {
+  size_t pos = filename.rfind(".");
+  if (pos != std::string::npos) {
+    return filename.substr(pos); // Get the extension (e.g., ".jpg")
+  }
+  return ".unknown";
+}
 
-    std::cout << "Server is running at http://localhost:8080\n";
-    svr.listen("0.0.0.0", 8080);
+int main() {
+  httplib::Server svr;
 
-    return 0;
+  svr.Post("/upload", [](const httplib::Request &req, httplib::Response &res) {
+    if (req.has_file("image")) {
+      const httplib::MultipartFormData &file = req.get_file_value("image");
+      string timestamp = get_unix_timestamp();
+      string extension = get_file_extension(file.filename);
+
+      cout << "Received file: " << file.filename << endl;
+      cout << "File size: " << file.content.length() << " bytes" << endl;
+      cout << "Current fs: " << fs::current_path() << endl;
+
+      string formatted_filename =
+          "../uploads/" + file.filename + "_" + timestamp + extension;
+      cout << "Formatted filename: " << formatted_filename << endl;
+
+      // save file to disk
+      ofstream out_file(formatted_filename, ios::binary);
+      out_file.write(file.content.c_str(), file.content.size());
+      if (!out_file.good()) {
+        res.set_content("Upload image failed", "text/plain");
+      } else {
+        res.set_content("Image uploaded succesfully", "text/plain");
+      }
+      out_file.close();
+    } else {
+      res.status = 400;
+      res.set_content("No Image File Provided", "text/plain");
+    }
+  });
+
+  cout << "Server is running at http://localhost:8080\n";
+  svr.listen("0.0.0.0", 8080);
+
+  return 0;
 }
